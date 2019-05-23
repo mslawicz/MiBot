0833c1d5a13e6ac617b8e7d9b1805663057d2ca3
Marcin <slawicz@echostar.pl>
2019-05-23 23:07:38 +0200

    LED and pushbutton test in main

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,34 @@
 #include "stm32f4xx.h"
 #include "stm32f4xx_nucleo.h"
 #include "system.h"
+#include "gpio.h"
 
 int main(void)
 {
     System::getInstance()->config();
 
+    GPIO pushbutton(USER_BUTTON_GPIO_PORT, USER_BUTTON_PIN, GPIO_MODE_INPUT, GPIO_PULLUP);
+    GPIO led(LED2_GPIO_PORT, LED2_PIN, GPIO_MODE_OUTPUT_PP);
+    while(1)
+    {
+        uint32_t delay;
+        if(pushbutton.read())
+        {
+            delay = 850;
+        }
+        else
+        {
+            delay = 450;
+        }
+        led.write(GPIO_PinState::GPIO_PIN_SET);
+        HAL_Delay(100);
+        led.write(GPIO_PinState::GPIO_PIN_RESET);
+        HAL_Delay(100);
+        led.write(GPIO_PinState::GPIO_PIN_SET);
+        HAL_Delay(100);
+        led.write(GPIO_PinState::GPIO_PIN_RESET);
+        HAL_Delay(delay);
+    }
 
     System::getInstance()->terminate();
 }
