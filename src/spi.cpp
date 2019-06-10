55de816ffe23614d61598b0c19fabf7827bb49a1
Marcin <slawicz@echostar.pl>
2019-06-10 19:21:52 +0200

    SPI files and classes created

---
+++ src/spi.cpp
@@ -0,0 +1,31 @@
+/*
+ * spi.cpp
+ *
+ *  Created on: 10.06.2019
+ *      Author: Marcin
+ */
+
+#include "spi.h"
+
+SpiBus::SpiBus()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+SpiBus::~SpiBus()
+{
+    // TODO Auto-generated destructor stub
+}
+
+
+SpiDevice::SpiDevice()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+SpiDevice::~SpiDevice()
+{
+    // TODO Auto-generated destructor stub
+}
---
+++ src/spi.h
@@ -0,0 +1,25 @@
+/*
+ * spi.h
+ *
+ *  Created on: 10.06.2019
+ *      Author: Marcin
+ */
+
+#ifndef SPI_H_
+#define SPI_H_
+
+class SpiBus
+{
+public:
+    SpiBus();
+    ~SpiBus();
+};
+
+class SpiDevice
+{
+public:
+    SpiDevice();
+    ~SpiDevice();
+};
+
+#endif /* SPI_H_ */
