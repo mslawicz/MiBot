af9a2c08963b3b03989239d2253f3777453158d2
Marcin <slawicz@echostar.pl>
2019-06-10 18:08:16 +0200

    Bluetooth class and files created

---
+++ src/bluetooth.cpp
@@ -0,0 +1,20 @@
+/*
+ * bluetooth.cpp
+ *
+ *  Created on: 10.06.2019
+ *      Author: Marcin
+ */
+
+#include "bluetooth.h"
+
+Bluetooth::Bluetooth()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+Bluetooth::~Bluetooth()
+{
+    // TODO Auto-generated destructor stub
+}
+
---
+++ src/bluetooth.h
@@ -0,0 +1,18 @@
+/*
+ * bluetooth.h
+ *
+ *  Created on: 10.06.2019
+ *      Author: Marcin
+ */
+
+#ifndef BLUETOOTH_H_
+#define BLUETOOTH_H_
+
+class Bluetooth
+{
+public:
+    Bluetooth();
+    ~Bluetooth();
+};
+
+#endif /* BLUETOOTH_H_ */
