20a6372bec8136332068e47a6c5d175512283c4a
Marcin <slawicz@echostar.pl>
2019-06-12 19:53:34 +0200

    eeprom files created

---
+++ src/eeprom.cpp
@@ -0,0 +1,20 @@
+/*
+ * eeprom.cpp
+ *
+ *  Created on: 12.06.2019
+ *      Author: Marcin
+ */
+
+#include "eeprom.h"
+
+Eeprom::Eeprom()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+Eeprom::~Eeprom()
+{
+    // TODO Auto-generated destructor stub
+}
+
---
+++ src/eeprom.h
@@ -0,0 +1,18 @@
+/*
+ * eeprom.h
+ *
+ *  Created on: 12.06.2019
+ *      Author: Marcin
+ */
+
+#ifndef EEPROM_H_
+#define EEPROM_H_
+
+class Eeprom
+{
+public:
+    Eeprom();
+    ~Eeprom();
+};
+
+#endif /* EEPROM_H_ */
